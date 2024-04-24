33909d292a32a715ac12fc945190d503dc5badf1
Leandro Lopes <leandro-lopes46@hotmail.com>
2024-04-24 16:10:53 -0300

    chore: add font sizes

--- src/presentation/styles/theme.ts
+++ src/presentation/styles/theme.ts
@@ -31,7 +31,19 @@ export const theme = {
 			body: 'var(--font-body)',
 		},
 		sizes: {
+			xs: '1.2rem',
+			sm: '1.4rem',
 			base: '1.6rem',
+			lg: '1.8rem',
+			xl: '2rem',
+			'2xl': '2.2rem',
+			'3xl': '2.4rem',
+			'4xl': '2.6rem',
+			'5xl': '2.8rem',
+			'6xl': '3.2rem',
+			'7xl': '3.6rem',
+			'8xl': '4rem',
+			'9xl': '5.5rem',
 		},
 	},
-};
+} as const;
