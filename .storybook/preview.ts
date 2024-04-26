43fe3959fcfc1d02e73bb5d8eaf0d3ec17d6f1bf
Leandro Lopes <leandro-lopes46@hotmail.com>
2024-04-25 22:41:02 -0300

    chore: add background colors on storybook parameters

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,31 +1,56 @@
 import type { Preview } from '@storybook/react';
 
-import { ThemeProvider } from 'styled-components';
 import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import { ThemeProvider } from 'styled-components';
 
-import { theme } from '../src/presentation/styles/theme';
 import { GlobalStyles } from '../src/presentation/styles/global';
+import { theme } from '../src/presentation/styles/theme';
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-  },
+	parameters: {
+		backgrounds: {
+			default: 'background-400',
+			values: [
+				{
+					name: 'background-100',
+					value: theme.colors.background[100],
+				},
+				{
+					name: 'background-200',
+					value: theme.colors.background[200],
+				},
+				{
+					name: 'background-300',
+					value: theme.colors.background[300],
+				},
+				{
+					name: 'background-400',
+					value: theme.colors.background[400],
+				},
+				{
+					name: 'background-500',
+					value: theme.colors.background[500],
+				},
+			],
+		},
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/,
+			},
+		},
+	},
 
-  decorators: [
-    withThemeFromJSXProvider({
-      themes: {
-        default: theme,
-      },
-      defaultTheme: 'default',
-      Provider: ThemeProvider,
-      GlobalStyles,
-    }),
-  ],
+	decorators: [
+		withThemeFromJSXProvider({
+			themes: {
+				default: theme,
+			},
+			defaultTheme: 'default',
+			Provider: ThemeProvider,
+			GlobalStyles,
+		}),
+	],
 };
 
 export default preview;
