b948744c3b708a4267ef041db55df7dbcf9e0b7e
Leandro Lopes <leandro-lopes46@hotmail.com>
2024-04-28 19:51:10 -0300

    feat: add container

---
+++ src/presentation/styles/container.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { media } from './helpers/media';
+
+export const Container = styled.div`
+  max-width: 128rem;
+  padding: 0 1.6rem;
+
+  ${media.xl`
+    padding: 0 8rem;
+  `}
+`;
