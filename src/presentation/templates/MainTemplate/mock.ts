362adb92c85e1c006b43676cc6030c817743fba3
Leandro Lopes <leandro-lopes46@hotmail.com>
2024-04-29 20:39:55 -0300

    feat: add MainTemplate

---
+++ src/presentation/templates/MainTemplate/mock.ts
@@ -0,0 +1,7 @@
+import { headerMock } from '@/presentation/components/Header/mock';
+
+import { MainTemplateProps } from '.';
+
+export const mainTemplateMock: MainTemplateProps = {
+	header: headerMock,
+};
