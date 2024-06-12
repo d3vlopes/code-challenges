a01778f260963fc7d9c1de2daabea8ae09753a5d
Leandro Lopes <leandro-lopes46@hotmail.com>
2024-06-12 20:00:22 -0300

    chore: change Logo mock

--- src/presentation/components/Logo/mock.ts
+++ src/presentation/components/Logo/mock.ts
@@ -1,6 +1,6 @@
 import { LogoProps } from '.';
 
 export const logoMock: LogoProps = {
-	src: 'https://sa-east-1.graphassets.com/clvfs1ld70bcs07ke07bkdxol/clvftxmtb0s1407m4c5ffxzu3',
+	src: 'https://sa-east-1.graphassets.com/clvfs1ld70bcs07ke07bkdxol/clxcfha33086207lv0j1wex8v',
 	alt: 'Duas tags HTML representando o código e os desafios com um quadrado no meio representando os devs do Code Challenges que estão entrem o código e os desafios.',
 };
