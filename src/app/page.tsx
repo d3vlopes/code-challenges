f0419e65f2df31e2a6f152f2cee50a7e80ab20d6
Leandro Lopes <leandro-lopes46@hotmail.com>
2024-06-13 15:19:45 -0300

    feat: add JSONLd on page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,15 @@ import { MainTemplate } from '@/presentation/templates/MainTemplate';
 
 import { mainTemplateMock } from '@/presentation/templates/MainTemplate/mock';
 
+import { JSONLd } from './seo';
+
 export default function Main() {
-	return <MainTemplate {...mainTemplateMock} />;
+	const data = mainTemplateMock;
+
+	return (
+		<>
+			<MainTemplate {...data} />
+			<JSONLd data={data} />
+		</>
+	);
 }
