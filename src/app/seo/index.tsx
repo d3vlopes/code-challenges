d917188f60247acf50f7c0f2e2748d484bf90f9a
Leandro Lopes <leandro-lopes46@hotmail.com>
2024-06-13 15:17:52 -0300

    feat: add SEO metadata

---
+++ src/app/seo/index.tsx
@@ -0,0 +1,65 @@
+import { MainModel } from '@/domain/models/Main';
+
+interface JsonLdProps {
+	data: MainModel;
+}
+
+export const seoConfig = {
+	title: 'Code Challenges - Experiência definitiva para desenvolvedores',
+	description:
+		'O Code Challenges é um ambiente inovador, projetado para fornecer experiência real aos desenvolvedores através de desafios baseados em testes técnicos reais enfrentados em empresas. O objetivo é oferecer uma experiência prática e imersiva, permitindo aos desenvolvedores aprimorar suas habilidades em um contexto que simula desafios do mundo real.',
+	url: 'https://www.codechallenges.com.br',
+	alternateName: 'Code Challenges',
+};
+
+export function generateJSONLd(data: MainModel) {
+	const organizationJSONLd = {
+		'@context': 'https://schema.org',
+		'@type': 'Organization',
+		logo: data.header.logo.src,
+		name: seoConfig.alternateName,
+		url: seoConfig.url,
+		email: 'contato.codechallenges@gmail.com',
+		founder: {
+			jobTitle: 'Founder | CEO',
+			name: 'Leandro Lopes',
+			email: 'contato.leandrolopes@outlook.com',
+		},
+	};
+
+	const productJSONLd = {
+		'@context': 'https://schema.org',
+		'@type': 'Product',
+		name: `Trilha ${seoConfig.alternateName}`,
+		description: seoConfig.description,
+		offers: {
+			'@type': 'Offer',
+			url: data.checkout.button.href,
+			priceCurrency: 'BRL',
+			price: data.checkout.salePrice,
+			availability: 'https://schema.org/InStock',
+		},
+	};
+
+	return {
+		organizationJSONLd,
+		productJSONLd,
+	};
+}
+
+export const JSONLd = ({ data }: JsonLdProps) => {
+	const { organizationJSONLd, productJSONLd } = generateJSONLd(data);
+
+	return (
+		<>
+			<script
+				type="application/ld+json"
+				dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationJSONLd) }}
+			/>
+			<script
+				type="application/ld+json"
+				dangerouslySetInnerHTML={{ __html: JSON.stringify(productJSONLd) }}
+			/>
+		</>
+	);
+};
