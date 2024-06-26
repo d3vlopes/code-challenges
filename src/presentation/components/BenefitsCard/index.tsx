1c65db5144e6a78de12c0b19ebbfdb4477a74054
Leandro Lopes <leandro-lopes46@hotmail.com>
2024-06-25 22:10:43 -0300

    fix: change BenefitsCard animation

--- src/presentation/components/BenefitsCard/index.tsx
+++ src/presentation/components/BenefitsCard/index.tsx
@@ -33,7 +33,7 @@ export const BenefitsCard = ({ items }: BenefitsCardProps) => {
 						whileInView="visible"
 						animate={inView ? 'visible' : 'hidden'}
 						variants={animationVariants}
-						viewport={{ once: true, amount: 'all' }}
+						viewport={{ once: true, amount: 'some' }}
 						transition={{
 							duration: 0.5,
 							delay: animationDelay,
